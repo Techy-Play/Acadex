6b1264a20b043993bd5415adb48485d9351763bf
Lokesh Paneru <lokeshpaneru20508@gmail.com>
2026-03-02 12:14:21 +0530

    V:2.0.9

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -17,7 +17,7 @@ import fs from "fs";
 function getAppUrl(): string {
   if (process.env.NEXT_PUBLIC_APP_URL) return process.env.NEXT_PUBLIC_APP_URL.replace(/\/$/, "");
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return "http://au-acadex.com";
+  return "https://au-acadex.com";
 }
 
 const transporter = nodemailer.createTransport({
